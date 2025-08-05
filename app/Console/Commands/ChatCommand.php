6f176d03887581d73c0c9c712cc212f0cb8cf587
Rameez Israr <rameez@govassist.com>
2025-08-05 20:15:07 +0500

    Update chat command to use prompts

--- app/Console/Commands/ChatCommand.php
+++ app/Console/Commands/ChatCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\AI\OpenAIChat;
 use function Laravel\Prompts\text;
 use function Laravel\Prompts\info;
+use function Laravel\Prompts\spin;
 
 class ChatCommand extends Command
 {
@@ -14,7 +15,7 @@ class ChatCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'chat';
+    protected $signature = 'chat {--system=}';
 
     /**
      * The console command description.
@@ -28,19 +29,26 @@ class ChatCommand extends Command
      */
     public function handle()
     {
-//         $question = $this->ask('What is your  question for AI');
-        $question = text('What is your  question for AI');
-
         $chat = new OpenAIChat();
 
-        $response = $chat->send($question);
+        if($this->option('system')){
+            $chat->systemMessage($this->option('system'));
+        }
+//         $question = $this->ask('What is your  question for AI');
+        $question = text(
+            label: 'What is your  question for AI',
+            required: true
+        );
 
+
+//         $response = $chat->send($question);
+            $response = spin(fn() => $chat->send($question), 'Sending request...');
 //        $this->info($response);
         info($response);
 
        while($this->ask('Do you want to respond?')){
         $question = text('What is your reply?');
-        $response = $chat->send($question);
+        $response = spin(fn() => $chat->send($question), 'Sending request...');
 
          info($response);
        }

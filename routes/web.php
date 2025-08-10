897f96f1319ce5df82246516559f7c5813473b3f
Rameez Israr <rameez@govassist.com>
2025-08-10 23:53:25 +0500

    Validate the text if its spam or not

--- routes/web.php
+++ routes/web.php
@@ -116,3 +116,37 @@ Route::post('/image', function () {
 });
 
 
+Route::get('/ai/spam-check', function () {
+    return view('spam-check');
+});
+
+Route::post('/ai/spam-check', function () {
+    $attributes = request()->validate([
+        'body' => ['required', 'string']
+    ]);
+
+    $response = \OpenAI\Laravel\Facades\OpenAI::chat()->create([
+        'model' =>  'gpt-4.1-nano',
+        'messages' => [
+            ['role' => 'system', 'content' => 'You are a forum moderator who always responds using JSON.'],
+            [
+                'role' => 'user',
+                'content' => <<<EOT
+                    Please inspect the following text and determine if it is spam.
+
+                    {$attributes['body']}
+
+                    Expected Response Example:
+
+                    {"is_spam": true|false}
+                    EOT
+            ]
+        ],
+        'response_format' => ['type' => 'json_object']
+    ])->choices[0]->message->content;
+
+    $response = json_decode($response);
+
+    // Trigger failed validation, display a flash message, abort...
+    return $response->is_spam ? 'THIS IS SPAM!': 'VALID POST';
+});
